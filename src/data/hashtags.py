58585b336d8301070272f140b149f22212f709db
lukasschr <lukas.schroeder@ewe.net>
2023-04-10 08:43:47 +0200

    extend function search_similar_hashtags

--- src/data/hashtags.py
+++ src/data/hashtags.py
@@ -1,13 +1,18 @@
 import snscrape.modules.twitter as sntwitter
+from tqdm import tqdm
+from datetime import date, timedelta
 
-def search_similar_hashtags(hashtags:list, limit:int=100):
+def search_similar_hashtags(hashtags:list, since=date.today()-timedelta(weeks=4), until=date.today(),
+                            limit:int=9999):
     """Search for similar hashtags.
-    
+
     Searches for hashtags that users frequently use in connection with the passed hashtags.
 
     Args:
         hastags: list of predefined hashtags. this must be specified in order to find other similar 
             ones.
+        since: start date for searching similar hashtags (default value: last four weeks)
+        until: end date for searching similar hashtags  (default value: last four weeks)
         limit: number of twitter posts to be searched for similar hashtags.
 
     Returns:
@@ -17,10 +22,10 @@ def search_similar_hashtags(hashtags:list, limit:int=100):
     last_element = hashtags.pop()
     for hashtag in hashtags:
         query = query+f'{str(hashtag)}, OR '
-    query = query+f'{str(last_element)})'
+    query = query+f'{str(last_element)}) '+f'until:{until} since:{since}'
 
     list_of_hashtags = []
-    for i, tweet in enumerate(sntwitter.TwitterSearchScraper(query).get_items()):
+    for i, tweet in tqdm(enumerate(sntwitter.TwitterSearchScraper(query).get_items()), total=limit):
         if i >= limit:
             break
         else:
