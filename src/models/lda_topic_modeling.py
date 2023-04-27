f7ff2ed24b5524c5916cd1a9257030082fbb52f5
lukasschr <lukas.schroeder@ewe.net>
2023-04-27 11:59:26 +0200

    create lda_topic_modeling.py with class Model

---
+++ src/models/lda_topic_modeling.py
@@ -0,0 +1,79 @@
+import time
+
+from gensim import corpora, models
+import pandas as pd
+
+
+class Model():
+    """LDA Topic Model.
+    
+    This class contains functions for creating, managing and evaluating topic models. 
+    It is an LDA topic model, which is calculated using gensim.
+    
+    Attributes:
+        text: A list of preprocessed text
+        multicore: Number of processors that can be used for the calculations ( > 0 = ldamulticore)
+    """
+    def __init__(self, text, multicore):
+        self.multicore = multicore
+        self.text = text
+
+
+    def build(self, **kwargs):
+        """Build the LDA model.
+
+        Calculates the LDA model. Depending on multicore, either method LdaModel or method 
+        LdaMulticore is selected.
+
+        Args:
+            kwargs: Takes all common parameters and their values that can be passed to the 
+            LdaMulticore function. Ex:
+                num_topics, alpha, eta, passes
+
+        Returns:
+            lda_model: the calculated LDA model
+            dictionary: the dictionary used
+            int(start_time): used seed/random_state
+            calculation_time: Time required for the calculation in minutes
+        """
+        dictionary = corpora.Dictionary(self.text) # create a dictionary/id2word
+        corpus = [dictionary.doc2bow(text) for text in self.text] # create a corpus
+
+        start_time = time.time()
+        if self.multicore == 0:
+            lda_model = models.LdaModel(corpus=corpus, id2word=dictionary, **kwargs, 
+                                        random_state=int(start_time))
+        else:
+            lda_model = models.ldamulticore.LdaMulticore(corpus=corpus, id2word=dictionary, 
+                                                         workers=self.multicore, **kwargs, 
+                                                         random_state=int(start_time))    
+        end_time = time.time()
+        calculation_time = (end_time - start_time) / 60  # in minutes
+        return lda_model, dictionary, int(start_time), calculation_time
+
+
+    def evaluate(self, model, text, dictionary):
+         """Calculates metrics for checking accuracy.
+
+         Calculates the following metrics: Coherence Score, ...
+
+         Args:
+            model: LDA Model
+            text: the text used to create the model
+            dictionary: the dictionary used to create the model
+
+         """
+         # calculate coherence score
+         coherence_model_lda = models.coherencemodel.CoherenceModel(model=model, texts=text, 
+                                                                    dictionary=dictionary, coherence='c_v')
+         coherence_score = coherence_model_lda.get_coherence()
+         # todo: more metrics ...
+         return coherence_score
+
+
+if __name__ == '__main__':
+    df = pd.read_feather(path='../../data/processed/twitter_tweets_processed.feather')
+    m = Model(text=df['preprocessed_text'], multicore=2)
+    lda_model, dictionary, seed, calculation_time = m.build(num_topics=10, alpha=0.1, eta=0.1, passes=1)
+    cs = m.evaluate(model=lda_model, text=df['preprocessed_text'], dictionary=dictionary)
+    print(cs)
